8b3257e478e86b4557dc9c8a19ff2a85419f08b7
Niels Taatgen <n.a.taatgen@rug.nl>
2018-03-05 14:16:23 +0100

    Initial coding, still lot to do.

--- Prisoner's Dilemma/Vision.swift
+++ Prisoner's Dilemma/Vision.swift
@@ -7,3 +7,69 @@
 //
 
 import Foundation
+
+class Vision {
+    let model: Model
+    
+    class VisualObject {
+        var name: String
+        var visualType: String
+        var value: String
+        var x, y, w, h, d : Double
+        var attended = false
+        var attendedTime = 0.0
+        var creationTime = 0.0
+        var visloc: Chunk? = nil
+        
+        init(name: String, visualType: String, value: String, x: Double, y: Double, w: Double, h: Double, d: Double) {
+            self.name = name
+            self.visualType = visualType
+            self.value = value
+            self.x = x + w/2
+            self.y = y + h/2
+            self.w = w
+            self.h = h
+            self.d = d
+        }
+    }
+    
+    
+    var visualAttentionLatency = 0.085
+    var visualNumFirst = 4.0
+    var bufferStuffing = true
+    
+    var visicon: [String:VisualObject] = [:]
+    
+    
+    init(model: Model) {
+        self.model = model
+    }
+    
+    func getVisualLocation(name: String) -> Chunk? {
+        if let vo = visicon[name] {
+            if vo.visloc != nil {
+                return vo.visloc!
+            } else {
+                let vislocChunk = model.generateNewChunk(string: name)
+                vislocChunk.setSlot(slot: "isa", value: "visual-location")
+                vislocChunk.setSlot(slot: "kind", value: vo.visualType)
+                vislocChunk.setSlot(slot: "screenx", value: vo.x)
+                vislocChunk.setSlot(slot: "screeny", value: vo.y)
+                vislocChunk.setSlot(slot: "width", value: vo.w)
+                vislocChunk.setSlot(slot: "height", value: vo.h)
+                vislocChunk.setSlot(slot: "distance", value: vo.d)
+                vo.visloc = vislocChunk
+                return vislocChunk
+            }
+        } else {
+            return nil
+        }
+    }
+    
+    func clearVisual() {
+        visicon = [:]
+    }
+    
+    
+    
+}
